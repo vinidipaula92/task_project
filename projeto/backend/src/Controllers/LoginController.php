4d5d2712fc4e57d0250f9756e88f2e7f76e7de3d
Vinicius Anjos <vinidipaula92@gmail.com>
2025-03-25 22:36:32 -0300

    feat: :sparkles: implementação de login

---
+++ projeto/backend/src/Controllers/LoginController.php
@@ -0,0 +1,60 @@
+<?php
+namespace Src\Controllers;
+
+use mysqli;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+
+class LoginController
+{
+    private $db_connection;
+
+    public function __construct()
+    {
+        $this->db_connection = new mysqli(
+            $_ENV['DB_HOST'],
+            $_ENV['DB_USER'],
+            $_ENV['DB_PASS'],
+            $_ENV['DB_NAME']
+        );
+
+        if ($this->db_connection->connect_error) {
+            die("Conexão com o banco de dados falhou: " . $this->db_connection->connect_error);
+        }
+    }
+
+    public function createUser(Request $request, Response $response)
+    {
+      $data = json_decode($request->getBody()->getContents(), true);
+      $password = $data['password'] = md5($data['password']);
+      $token = md5(uniqid(rand(), true));
+      $query = $this->db_connection->prepare("INSERT INTO users (name, email, password, token, created_at, updated_at) VALUES (?, ?, ?, ?, now(), now())");
+      $query->bind_param("ssss", $data['name'], $data['email'], $password, $token);
+      if ($query->execute()) {
+        $response = $response->withHeader('Content-Type', 'application/json');
+        $response->getBody()->write(json_encode(['message' => 'Usuário criado com sucesso']));
+        return $response->withStatus(201);
+      } else {
+          $response = $response->withHeader('Content-Type', 'application/json');
+          $response->getBody()->write(json_encode(['error' => 'Falha ao criar o usuário']));
+          return $response->withStatus(500);
+      }
+
+    }
+
+    public function getUser(Response $response): Response
+    {
+      $query = "SELECT * FROM users";
+      $result = $this->db_connection->query($query);
+
+      $user = [];
+      while ($row = $result->fetch_assoc()) {
+          $user[] = $row;
+      }
+
+      $response = $response->withHeader('Content-Type', 'application/json');
+      $response->getBody()->write(json_encode($user));
+      return $response;
+    }
+}
--- projeto/backend/src/Routes/routes.php
+++ projeto/backend/src/Routes/routes.php
@@ -2,11 +2,13 @@
 use Slim\App;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Src\Controllers\LoginController;
 use Src\Controllers\TaskController;
 use Src\Controllers\ValidateToken;
 
 return function (App $app) {
   $taskController = new TaskController();
+  $loginController = new LoginController();
 
   $app->get('/tasks', function (Request $request, Response $response) use ($taskController) {
     // $token = $request->getHeaderLine('Authorization');
@@ -30,5 +32,23 @@ return function (App $app) {
   });
   $app->delete('/tasks/{id}', function (Request $request, Response $response, $args) use ($taskController) {
       return $taskController->deleteTask( $response, $args['id']);
+  });
+
+  $app->get('/users', function (Request $request, Response $response) use ($loginController) {
+    // $token = $request->getHeaderLine('Authorization');
+    // if (empty($token)) {
+    //   $response->getBody()->write('{"error": "Token não informado"}');
+    //   return $response->withStatus(401);
+    // }
+    // $validateToken = new ValidateToken();
+    // $user = $validateToken->ValidateKey($token);
+    // if (empty($user)) {
+    //   $response->getBody()->write('{"error": "Token inválido"}');
+    //   return $response->withStatus(401);
+    // }
+    return $loginController->getUser($response);
+  });
+  $app->post('/users', function (Request $request, Response $response) use ($loginController) {
+      return $loginController->createUser($request, $response);
   });
 };
